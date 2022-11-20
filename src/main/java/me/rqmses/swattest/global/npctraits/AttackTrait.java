21de8852541777b391f47ac80d28c7d7ec3747cc
Ramses <isuramses@gmail.com>
2022-11-20 05:03:23 +0100

    commit

--- src/main/java/me/rqmses/swattest/global/npctraits/AttackTrait.java
+++ src/main/java/me/rqmses/swattest/global/npctraits/AttackTrait.java
@@ -3,16 +3,14 @@ package me.rqmses.swattest.global.npctraits;
 import me.rqmses.swattest.SWATtest;
 import me.rqmses.swattest.global.Items;
 import me.rqmses.swattest.listeners.PlayerInteractListener;
-import net.citizensnpcs.api.ai.TargetType;
 import net.citizensnpcs.api.event.NPCDeathEvent;
+import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.api.trait.Trait;
 import net.citizensnpcs.api.trait.TraitName;
 import org.bukkit.*;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.PotionSplashEvent;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.*;
@@ -30,73 +28,79 @@ public class AttackTrait extends Trait {
 
     @Override
     public void onSpawn() {
-        Player npcplayer = (Player) npc.getEntity();
-        npcplayer.setGameMode(GameMode.SURVIVAL);
-        ((CraftPlayer) npcplayer).setMaxHealth(40);
-        npcplayer.setHealth(40);
+        NPC npcplayer = getNPC();
+        ((Player) npcplayer.getEntity()).setGameMode(GameMode.SURVIVAL);
+        //noinspection deprecation
+        ((Player) npcplayer.getEntity()).setMaxHealth(40);
+        ((Player) npcplayer.getEntity()).setHealth(40);
 
-        npc.setProtected(false);
+        npcplayer.setProtected(false);
 
         result = null;
 
-        taskID1.put(npc.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(SWATtest.plugin, () -> {
+        taskID1.put(npcplayer.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(SWATtest.plugin, () -> {
             double lastDistance = Double.MAX_VALUE;
             result = null;
-            for (Player p : npcplayer.getWorld().getPlayers()) {
-                if (!((Player) npc.getEntity()).hasLineOfSight(p)) {
+            for (Player p : ((Player) npcplayer.getEntity()).getWorld().getPlayers()) {
+                if (!((Player) npcplayer.getEntity()).hasLineOfSight(p)) {
                     continue;
                 }
                 if (p.hasMetadata("NPC")) {
                     continue;
                 }
                 if (p.getGameMode() == GameMode.SURVIVAL) {
-                    double distance = npc.getEntity().getLocation().distance(p.getLocation());
+                    double distance = npcplayer.getEntity().getLocation().distance(p.getLocation());
                     if (distance > 25) {
                         continue;
                     }
                     if (distance < lastDistance) {
                         lastDistance = distance;
                         result = p;
-                        if (!npcplayer.getInventory().contains(Material.DIAMOND_BARDING)) {
-                            npcplayer.getInventory().setItemInMainHand(Items.getM4());
+                        if (!((Player) npcplayer.getEntity()).getInventory().contains(Material.DIAMOND_BARDING)) {
+                            ((Player) npcplayer.getEntity()).getInventory().setItemInMainHand(Items.getM4());
                         }
-                        npc.getNavigator().setTarget(result, true);
-                        npc.getNavigator().getDefaultParameters().useNewPathfinder(true);
-                        npc.getNavigator().getDefaultParameters().avoidWater(true);
-                        ((Player) npc.getEntity()).setSprinting(true);
+                        npcplayer.getNavigator().setTarget(result, true);
+                        npcplayer.getNavigator().getDefaultParameters().useNewPathfinder(true);
+                        npcplayer.getNavigator().getDefaultParameters().avoidWater(true);
+                        ((Player) npcplayer.getEntity()).setSprinting(true);
                     }
                 }
             }
             if (result == null) {
-                npc.getNavigator().cancelNavigation();
+                npcplayer.getNavigator().cancelNavigation();
             }
         }, 60L, 60L));
 
-        taskID2.put(npc.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(SWATtest.plugin, () -> {
+        taskID2.put(npcplayer.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(SWATtest.plugin, () -> {
             if (result != null) {
                 boolean sight = true;
-                if (((CraftPlayer) npc.getEntity()).hasPotionEffect(PotionEffectType.BLINDNESS) || ((CraftPlayer) npc.getEntity()).hasPotionEffect(PotionEffectType.CONFUSION)) {
-                    npc.getNavigator().setTarget(result.getLocation());
+                if (((CraftPlayer) npcplayer.getEntity()).hasPotionEffect(PotionEffectType.BLINDNESS) || ((CraftPlayer) npcplayer.getEntity()).hasPotionEffect(PotionEffectType.CONFUSION)) {
+                    npcplayer.getNavigator().setTarget(result.getLocation());
                 } else {
-                    npc.faceLocation(result.getLocation());
-                    sight = ((Player) npc.getEntity()).hasLineOfSight(result);
+                    npcplayer.faceLocation(result.getLocation());
+                    sight = ((Player) npcplayer.getEntity()).hasLineOfSight(result);
                 }
                 if (result != null) {
                     if (sight) {
-                        PlayerInteractListener.shootM4((Player) npc.getEntity());
+                        PlayerInteractListener.shootM4((Player) npcplayer.getEntity());
                     }
-                    if (((Player) npc.getEntity()).getHealth() < 30) {
-                        ((Player) npc.getEntity()).chat("/use Kokain");
+                    if (((Player) npcplayer.getEntity()).getHealth() < 30) {
+                        ((Player) npcplayer.getEntity()).chat("/use Kokain");
                     }
                 }
             }
         }, 60L, 10L));
     }
 
+    @Override
+    public void onDespawn() {
+        Bukkit.getScheduler().cancelTask(taskID1.get(getNPC().getUniqueId()));
+        Bukkit.getScheduler().cancelTask(taskID2.get(getNPC().getUniqueId()));
+    }
+
     @EventHandler
     public void onDeath(NPCDeathEvent event) {
-        Bukkit.getScheduler().cancelTask(taskID1.get(event.getNPC().getUniqueId()));
-        Bukkit.getScheduler().cancelTask(taskID2.get(event.getNPC().getUniqueId()));
+        event.getNPC().despawn();
     }
 }
 
