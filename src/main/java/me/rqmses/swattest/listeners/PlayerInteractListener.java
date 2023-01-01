22793ff78102b5cac858d126d8394d34e36e478a
Ramses <isuramses@gmail.com>
2023-01-01 07:18:29 +0100

    commit

--- src/main/java/me/rqmses/swattest/listeners/PlayerInteractListener.java
+++ src/main/java/me/rqmses/swattest/listeners/PlayerInteractListener.java
@@ -92,12 +92,11 @@ public class PlayerInteractListener implements Listener {
       }
     }
     if (!Objects.equals(event.getPlayer().getInventory().getItemInMainHand().getType(), Material.AIR)) {
-      if (itemtoggles.containsKey(event.getItem().getType())) {
-        if (!itemtoggles.get(event.getItem().getType())) {
-          event.getPlayer().sendMessage("Dieses Item ist deaktiviert!");
-          event.setCancelled(true);
-          return true;
-        }
+      itemtoggles.putIfAbsent(event.getItem().getType(), Boolean.TRUE);
+      if (!itemtoggles.get(event.getItem().getType())) {
+        event.getPlayer().sendMessage("Dieses Item ist deaktiviert!");
+        event.setCancelled(true);
+        return true;
       }
     }
     if (player.getGameMode() == GameMode.SPECTATOR)
